9db2202111025c36e235fc68a62a04a970f12a39
Anthony Watson <anthonywatson@anthony-watsons-macbook-pro.local>
2018-12-06 11:37:17 -0600

    Updates styles for Post Feed

--- client/src/components/common/PostItem.jsx
+++ client/src/components/common/PostItem.jsx
@@ -33,16 +33,15 @@ class PostItem extends Component {
     return (
       <div className="card card-body mb-3">
         <div className="row">
-          <div className="col-md-2">
+          <div className="col-md-2 avatar-pic">
             <a href="profile.html">
               <img
-                className="rounded-circle d-none d-md-block"
+                className="rounded-circle d-md-block"
                 src={post.avatar}
                 alt=""
               />
             </a>
-            <br />
-            <p className="text-center">{post.firstname}</p>
+            <p>{post.firstname}</p>
           </div>
           <div className="col-md-10">
             <p className="lead">{post.text}</p>
